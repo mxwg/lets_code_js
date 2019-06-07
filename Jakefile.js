d49aa2e3c582c71eac21e542f61e7b2178378209
Maximilian Wenger <mwenger@uos.de>
2019-06-07 23:39:58 +0200

    Initial support for lint

---
+++ Jakefile.js
@@ -0,0 +1,18 @@
+/*global desc, task, jake, fail, complete */
+"use strict";
+
+task("default", ["lint"]);
+
+desc("Lint everything (but modules)");
+task("lint", [], function() {
+  var lint = require("./build/lint/lint_runner.js");
+  var files = new jake.FileList();
+  files.include("**/*.js");
+  files.exclude("node_modules");
+
+  var options = {
+    node: true
+  };
+
+  lint.validateFileList(files.toArray(), options, {});
+});
