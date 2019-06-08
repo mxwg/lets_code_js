3f631ab80a8c03148e38122d72a8ff0ee830d6cf
Maximilian Wenger <mwenger@uos.de>
2019-06-08 10:50:58 +0200

    Get Jakefile up-to-date, fail if lint fails

--- Jakefile.js
+++ Jakefile.js
@@ -1,18 +1,40 @@
 /*global desc, task, jake, fail, complete */
-"use strict";
+(function() {
+    "use strict";
 
-task("default", ["lint"]);
+    desc("Build and test");
+    task("default", ["lint"]);
 
-desc("Lint everything (but modules)");
-task("lint", [], function() {
-  var lint = require("./build/lint/lint_runner.js");
-  var files = new jake.FileList();
-  files.include("**/*.js");
-  files.exclude("node_modules");
+    desc("Lint everything (but modules)");
+    task("lint", [], function () {
+        var lint = require("./build/lint/lint_runner.js");
 
-  var options = {
-    node: true
-  };
+        var files = new jake.FileList();
+        files.include("**/*.js");
+        files.exclude("node_modules");
 
-  lint.validateFileList(files.toArray(), options, {});
-});
+        var options = nodeLintOptions();
+        var passed = lint.validateFileList(files.toArray(), options, {});
+        if (!passed) fail("Lint failed!");
+    });
+
+    function nodeLintOptions() {
+        return {
+            bitwise: true,
+            curly: false,
+            eqeqeq: true,
+            forin: true,
+            immed: true,
+            latedef: false,
+            newcap: true,
+            noarg: true,
+            noempty: true,
+            nonew: true,
+            regexp: true,
+            undef: true,
+            strict: true,
+            trailing: true,
+            node: true
+        };
+    }
+}());
