4f95247a09d7bd04a9e4f8662f7cb3297606b643
Maximilian Wenger <mwenger@uos.de>
2019-06-09 23:19:47 +0200

    Add drawing line

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -1,31 +1,49 @@
-/* global expect, wwp */
+/* global expect, wwp, $ */
 (function () {
     "use strict";
 
-
     describe("Drawing area", function () {
-        it("should be initialized in correct div", function () {
-            const drawingAreaDivId = "wwp-drawing-area";
-            const drawingArea = $("<div id='" + drawingAreaDivId + "'>content</div>");
+        function addDivToDocument(divString) {
+            const drawingArea = $(divString);
             $(document.body).append(drawingArea);
+            return drawingArea;
+        }
 
-            wwp.initializeDrawingArea(drawingAreaDivId);
+        it("should be initialized in correct div", function () {
+            const drawingArea = addDivToDocument("<div id='wwp-drawing-area'>content</div>");
 
+            wwp.initializeDrawingArea("wwp-drawing-area");
 
-            const tag = drawingArea.children(0)[0].tagName;
-            expect(tag).to.equal("svg");
+            const tagInsideDrawingArea = drawingArea.children(0)[0].tagName;
+            expect(tagInsideDrawingArea).to.equal("svg");
         });
 
         it("should have the same dimension as its enclosing div", function () {
             // create drawingArea as a jquery object
-            const drawingArea = $("<div style='height: 333px; width: 666px'>content</div>");
-            $(document.body).append(drawingArea);
+            const drawingArea = addDivToDocument("<div style='height: 333px; width: 666px'>content</div>");
 
             // get dom object out of jquery object using [0]
             const paper = wwp.initializeDrawingArea(drawingArea[0]);
 
             expect(paper.height).to.equal(333);
             expect(paper.width).to.equal(666);
+        });
+
+        it("should draw a line", function () {
+            const drawingArea = addDivToDocument("<div style='height: 300px; width: 500px'>content</div>");
+            const paper = wwp.initializeDrawingArea(drawingArea);
+
+            wwp.drawLine(20, 30, 200, 50);
+
+            let elements = [];
+            paper.forEach(function (element) {
+                elements.push(element);
+            });
+            expect(elements.length).to.equal(1);
+
+            const element = elements[0];
+            const path = element.node.attributes.d.value;
+            expect(path).to.equal("M20,30L200,50");
         });
     });
 
--- src/client/client.js
+++ src/client/client.js
@@ -6,10 +6,17 @@ wwp = {};
     "use strict";
 
     const raphael = Raphael;
+    let paper;
 
     wwp.initializeDrawingArea = function (drawingArea) {
-        const paper = raphael(drawingArea);
+        paper = raphael(drawingArea);
 
         return paper;
+    };
+
+    wwp.drawLine = function (x1, y1, x2, y2) {
+        // example command: "M10 10L90 90"
+        const stringCommand = `M${x1} ${y1}L${x2} ${y2}`;
+        const line = paper.path(stringCommand);
     };
 }());
